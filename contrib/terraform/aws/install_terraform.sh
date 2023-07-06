abcdc98dcac53e9afd232db5e7cef7281968929f
Anh Nguyen <anhnguyenviet.vn@gmail.com>
2023-07-06 13:46:44 +0700

    Update install_terraform.sh

--- contrib/terraform/aws/install_terraform.sh
+++ contrib/terraform/aws/install_terraform.sh
@@ -1,6 +1,9 @@
+sudo yum update -y
 sudo yum install -y yum-utils
 sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
 sudo yum -y install terraform
 terraform -help
 touch ~/.bashrc
 terraform -install-autocomplete
+sudo amazon-linux-extras install docker
+nohup dockerd &
