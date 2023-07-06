e387a885e1e1764614a79893b648ea1288ae47d7
Anh Nguyen <anhnguyenviet.vn@gmail.com>
2023-07-06 10:46:26 +0700

    Fix user_data script in create-infrastructure.tf

--- contrib/terraform/aws/create-infrastructure.tf
+++ contrib/terraform/aws/create-infrastructure.tf
@@ -72,7 +72,7 @@ apt install -y apt-transport-https ca-certificates curl software-properties-comm
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
 apt-cache policy docker-ce
-apt install -y docker-ce git vi vim
+apt install -y docker-ce git
 systemctl start docker
 usermod -aG docker ubuntu
 su - ubuntu
